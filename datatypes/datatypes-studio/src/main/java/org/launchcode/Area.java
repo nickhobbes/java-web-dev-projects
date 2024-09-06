cae2cc7edff45df78f630082fb8bc321feb9da59
Nicholas Cerame <nickcerame@Nicholass-MacBook-Air.local>
2024-09-05 20:25:53 -0500

    Finished studio

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,4 +1,21 @@
 package org.launchcode;
 
+import java.util.Scanner;
+
 public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double radius = 0;
+        do {
+            System.out.println("What is the radius: ");
+            if(input.hasNextDouble()) {
+                radius = input.nextDouble();
+            }
+            input.nextLine();
+        } while (radius <= 0);
+
+        double area = Circle.getArea(radius);
+        System.out.println("The area of the circle is " + area);
+    }
 }
--- datatypes/datatypes-studio/src/main/java/org/launchcode/Circle.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Circle.java
@@ -1,4 +1,7 @@
 package org.launchcode;
 
 public class Circle {
+    public static Double getArea(Double radius) {
+        return Math.PI * Math.pow(radius, radius);
+    }
 }
